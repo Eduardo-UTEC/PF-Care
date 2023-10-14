e9baac17c3225b26ee3ebb039b110bf48af64e2b
eduardo_UTEC <eduardo.olivera@estudiantes.utec.edu.uy>
2023-10-14 00:35:31 -0300

    se agregan 2 controllers a Patient: exist cedula y exist mail

--- care/src/main/java/uy/com/pf/care/model/documents/Patient.java
+++ care/src/main/java/uy/com/pf/care/model/documents/Patient.java
@@ -18,7 +18,7 @@ import java.util.List;
 @Document("Patients")
 @CompoundIndexes({
         @CompoundIndex(def = "{'identificationDocument':1, 'zone.countryName':1}", unique = true),
-        @CompoundIndex(def = "{'mail':1}", unique = true),
+        @CompoundIndex(def = "{'mail':1}"), // los mails pueden omitirse, con lo cual debe aceptar nulos repetidos
         @CompoundIndex(def = "{'zone.countryName':1, 'zone.departmentName':1, 'zone.cityName':1, 'zone.neighborhoodName':1, " +
                         "'name1':1}")
 })
